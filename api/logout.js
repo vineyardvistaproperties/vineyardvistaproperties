f5baadea9aa2216b837a85da1e31b113ba3f32c4
vineyardvistaproperties <vineyardvistaproperties@gmail.com>
2026-09-24 18:01:54 -0400

    Delete api/logout.js

--- api/logout.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req,res){
-  const area=req.query?.area||'site';
-  const cookies=[];
-  if(area==='management') cookies.push('vvp_management=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
-  else {cookies.push('vvp_site=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');cookies.push('vvp_management=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0')}
-  res.setHeader('Set-Cookie',cookies);
-  res.redirect(302,'/login.html');
-}
